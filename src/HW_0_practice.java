77d50853b76826ca88d4575c9c280a8825d48c99
oded-kapeta <oded.kapeta@campus.technion.ac.il>
2023-04-24 13:03:22 +0300

    main and practice 1

--- src/HW_0_practice.java
+++ src/HW_0_practice.java
@@ -1,2 +1,78 @@
-package PACKAGE_NAME;public class HW_0_practice {
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+public class HW_0_practice {
+    public static Scanner scanner;
+    public static void battleshipGame() {
+        // TODO: Add your code here (and add more methods).
+        String boardSize = new String();
+        boardSize = getBoardSize(boardSize);
+        int[] sizeOfBoard = new int[2];
+        splitBoard(sizeOfBoard,boardSize);
+        String battleships = new String();
+        battleships  = getBattleships(battleships);
+        String[] battleships_Array = battleships.split(" ");
+        int[][] battleships_Length = new int[battleships_Array.length][2];
+        splitBattleships(battleships_Length,battleships_Array);
+        print_all_Ships(battleships_Length);
+
+    }
+
+    public static String getBoardSize(String boardSize) {
+        System.out.println("Enter the board size");
+        boardSize = scanner.nextLine();
+        return  boardSize;
+    }
+    public static void splitBoard(int[]arr, String boardSize){
+        String[] temp = boardSize.split("X");
+        arr[0] = Integer.parseInt(temp[0]);
+        arr[1] = Integer.parseInt(temp[1]);
+    }
+
+    /**
+     * gets the sizes and amount of the ships
+     * @param battleships
+     * @return
+     */
+
+    public static String getBattleships(String battleships) {
+        System.out.println("Enter the battleships size :");
+        battleships = scanner.nextLine();
+        return battleships;
+    }
+
+    /**
+     * splits the sizes and amounts of the ships into an array
+     * @param arr
+     * @param arr2
+     */
+    public static void splitBattleships(int arr[][],String arr2[]){
+        for (int i = 0 ; i < arr2.length; i++){
+            String [] tempArray = arr2[i].split("X");
+            int row = Integer.parseInt(tempArray[0]);
+            int col = Integer.parseInt(tempArray[1]);
+            arr[i][0] = row;
+            arr[i][1] = col;
+        }
+    }
+
+    /**
+     * prints the amount and size of each ship
+     * @param arr
+     */
+    public static void print_all_Ships(int arr[][]){
+        for (int i = 0 ; i < arr.length;i++){
+            System.out.println();
+            for (int j = 0 ;j < arr[0].length;j++){
+                System.out.print(arr[i][j] + " ");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+        battleshipGame();
+
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,74 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static Scanner scanner;
+    public static Random rnd;
+
+    public static void battleshipGame() {
+        // TODO: Add your code here (and add more methods).
+        String boardSize = new String();
+        boardSize = getBoardSize(boardSize);
+        int[] sizeOfBoard = new int[2];
+        splitBoard(sizeOfBoard,boardSize);
+        String battleships = new String();
+        battleships  = getBattleships(battleships);
+        String[] battleships_Array = battleships.split(" ");
+        int[][] battleships_Length = new int[battleships_Array.length][2];
+        splitBattleship(battleships_Length,battleships_Array);
+    }
+
+    public static String getBoardSize(String boardSize) {
+        System.out.println("Enter the board size");
+        boardSize = scanner.nextLine();
+        return  boardSize;
+    }
+    public static void splitBoard(int[]arr, String boardSize){
+        String[] temp = boardSize.split("X");
+        arr[0] = Integer.parseInt(temp[0]);
+        arr[1] = Integer.parseInt(temp[1]);
+    }
+
+    public static String getBattleships(String battleships) {
+        System.out.println("Enter the battleships size :");
+        battleships = scanner.nextLine();
+        return battleships;
+    }
+    public static void splitBattleship(int arr[][],String arr2[]){
+        for (int i = 0 ; i < arr2.length; i++){
+            String [] tempArray = arr2[i].split("X");
+            int row = Integer.parseInt(tempArray[0]);
+            int col = Integer.parseInt(tempArray[1]);
+            arr[i][0] = row;
+            arr[i][1] = col;
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        String path = args[0];
+        scanner = new Scanner(new File(path));
+        battleshipGame();
+        int numberOfGames = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Total of " + numberOfGames + " games.");
+
+        for (int i = 1; i <= numberOfGames; i++) {
+            scanner.nextLine();
+            int seed = scanner.nextInt();
+            rnd = new Random(seed);
+            scanner.nextLine();
+            System.out.println("Game number " + i + " starts.");
+            battleshipGame();
+            System.out.println("Game number " + i + " is over.");
+            System.out.println("------------------------------------------------------------");
+        }
+        System.out.println("All games are over.");
     }
 }
+
+
+
